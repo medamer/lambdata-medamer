b0a6fc098b31377353d6b63829110d2fade03421
medamer <edamer.mo@gmail.com>
2020-06-01 19:37:42 -0700

    prepare the dist of the package

---
+++ setup.py
@@ -0,0 +1,20 @@
+# setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata-medamer", # the name that you will install via pip
+    version="1.0",
+    author="mohamed edamer",
+    author_email="edamer.mo@gmail.com",
+    description="not now, just testing",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    license="MIT",
+    url="https://github.com/medamer/lambdata-medamer",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
