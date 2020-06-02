19e43da987d4e91942969db7950b10689171028f
medamer <edamer.mo@gmail.com>
2020-06-01 18:51:26 -0700

    configured env, create a function

---
+++ my_mod.py
@@ -0,0 +1,21 @@
+# Import libraries:
+import pandas as pd
+from scikit-learn.model_selection import train_test_split
+
+# function to clean te dataset:
+
+def profile(df):
+    # Check and print missing values:
+    nans = df.isnull().sum()
+    nans = pd.DataFrame(nans, columns=['NaNs'])
+    print('Missing Values :')
+
+    # Make the train/test split:
+    train, test = train_test_split(df, test_size=0.2, stratify=df['Group'], random_state=42)
+    train, val = train_test_split(train, train_size=0.8, test_size=0.2, stratify=train['Group'], random_state=42)
+    print('\n')
+    print('Train shape     : ', train.shape)
+    print('Validation shape: ', val.shape)
+    print('Test shape      : ', test.shape)
+    print('\n')
+    print(df.head())
