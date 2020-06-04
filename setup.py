f52b85ef28d0abd7e7882b42f31cc45d2eda95b9
medamer <edamer.mo@gmail.com>
2020-06-03 20:30:48 -0700

    New version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdatamedamer", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="mohamed edamer",
     author_email="edamer.mo@gmail.com",
     description="not now, just testing",
