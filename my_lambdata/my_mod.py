044152ce787492217da0ccdfa85a56f202bf79a3
medamer <edamer.mo@gmail.com>
2020-06-03 19:36:20 -0700

    adding class

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,27 +1,37 @@
 # Import libraries:
 import pandas as pd
-#from scikit-learn.model_selection import train_test_split
+from scikit-learn.model_selection import train_test_split
 
+
+
+class Data:
 # function to clean te dataset:
-def profile(df):
-    # Check and print missing values:
-    nans = df.isnull().sum()
-    nans = pd.DataFrame(nans, columns=['NaNs'])
-    print('Missing Values :')
-    
-    #Describe dataset:
-    print('\n')
-    print('Describe dataset', df.describe().T)
+    def profile(df):
+        # Check and print missing values:
+        """
+        Param df is dataframe
+        Function checks for NaNs, print them, and print the description.
+        """
+        nans = df.isnull().sum()
+        nans = pd.DataFrame(nans, columns=['NaNs'])
+        print('Missing Values : \n', nans)
+        
+        #Describe dataset:
+        print('\n')
+        print('Describe dataset : \n', df.describe().T)
 
-    # Make the train/test split:
-    #train, test = train_test_split(df, test_size=0.2, stratify=df['Group'], random_state=42)
-    #train, val = train_test_split(train, train_size=0.8, test_size=0.2, stratify=train['Group'], random_state=42)
-    #print('\n')
-    #print('Train shape     : ', train.shape)
-    #print('Validation shape: ', val.shape)
-    #print('Test shape      : ', test.shape)
-    print('\n')
-    print(df.head())
+    def split(df):
+        # Make the train/test split:
+        """
+        Param df is dataframe.
+        Function split the dataframe into train, validation and test.
+        """
+        train, test = train_test_split(df, test_size=0.2, stratify=df['Group'], random_state=42)
+        train, validation = train_test_split(train, train_size=0.8, test_size=0.2, stratify=train['Group'], random_state=42)
+        print('\n')
+        print('Train shape     : ', train.shape)
+        print('Validation shape: ', validation.shape)
+        print('Test shape      : ', test.shape)
 
 
 """
