2d4cb05333b079eedef1d68f445e943156253537
medamer <edamer.mo@gmail.com>
2020-06-03 19:57:57 -0700

    fixed code style

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -3,20 +3,22 @@ import pandas as pd
 from scikit-learn.model_selection import train_test_split
 
 
-
 class Data:
-# function to clean te dataset:
+    # function to clean te dataset:
     def profile(df):
-        # Check and print missing values:
+
         """
         Param df is dataframe
         Function checks for NaNs, print them, and print the description.
         """
         nans = df.isnull().sum()
         nans = pd.DataFrame(nans, columns=['NaNs'])
+
+        # Check and print missing values:
         print('Missing Values : \n', nans)
-        
-        #Describe dataset:
+
+        # Describe dataset:
+
         print('\n')
         print('Describe dataset : \n', df.describe().T)
 
@@ -26,17 +28,18 @@ class Data:
         Param df is dataframe.
         Function split the dataframe into train, validation and test.
         """
-        train, test = train_test_split(df, test_size=0.2, stratify=df['Group'], random_state=42)
-        train, validation = train_test_split(train, train_size=0.8, test_size=0.2, stratify=train['Group'], random_state=42)
+        train, test = train_test_split(
+                                        df, test_size=0.2,
+                                        stratify=df['Group'],
+                                        random_state=42
+                                        )
+        train, validation = train_test_split(
+                                                train, train_size=0.8,
+                                                test_size=0.2,
+                                                stratify=train['Group'],
+                                                random_state=42
+                                                )
         print('\n')
         print('Train shape     : ', train.shape)
         print('Validation shape: ', validation.shape)
         print('Test shape      : ', test.shape)
-
-
-"""
-
-def test(x):
-    print(x*10)
-
-"""
